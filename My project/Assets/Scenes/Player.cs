e03ea794317fef4a6bbd7f6a49c8e7826978ac5c
Luiz carlos da silva filho <filho.luiz@academico.ifrn.edu.br>
2023-07-14 09:03:12 -0300

    luiz fez codigo do player

--- My project/Assets/Scenes/Player.cs
+++ My project/Assets/Scenes/Player.cs
@@ -1,18 +1,104 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int health = 3 ;
+    public float speed;
+    public float jumpForce;
+    
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    private bool isJumping;
+
+    private bool doubleJump;
+    private bool isFire;
+    
+    public bool isRight;
+
     // Start is called before the first frame update
     void Start()
     {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Mover();
+    }
+
+    void Mover()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+
+        if (movement > 0)
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+
+            isRight = true;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+
+        if (movement < 0)
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transition", 1);
+            }
+
+            isRight = false;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (movement == 0 && !isJumping)
+        {
+            anim.SetInteger("transition", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            anim.SetInteger("transition", 2);
+            rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            doubleJump = true;
+            isJumping = true;
+        }
+        else
+        {
+            if (doubleJump)
+            {
+                anim.SetInteger("transition", 2);
+                rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    { 
+        if (coll.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
     }
 }
